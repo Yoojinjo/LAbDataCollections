c6f7c05957cc2419fdc57eb6f91d3a55d3039f0c
Yoojinjo <eugenejo111@gmail.com>
2024-08-06 18:17:40 -0500

    feeling loopy

--- lab.js
+++ lab.js
@@ -1,2 +1,32 @@
 console.log("ready")
 
+//Original string
+const csv =
+("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26");
+
+console.log(csv)
+
+//Feeling Loopy exercise
+// iterate over each char in string, log results
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+let cell = "";
+let row = [];
+let data = [];
+for (let i = 0; i < csv.length; i++) {
+    let current_char = csv[i];
+    if (current_char !== "," && current_char !== "\n") {
+        cell = cell + current_char;
+    }
+    //when you encounter "," or "\n" or end of string, log the cell
+    if ("," === current_char || "\n" === current_char || i == csv.length - 1) {
+        row.push(cell);
+        cell = "";
+    }
+    // When you encounter the “\n” sequence or end of string, move to the next “row.
+    if ("\n" === current_char || i === csv.length - 1) {
+        data.push(row);
+        row = [];
+    }
+}
+console.log(data);
